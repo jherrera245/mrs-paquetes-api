87ca99b444fe14747d58293ab6b767c93facd1aa
jherrera245 <245jh17@gmail.com>
2024-04-20 16:43:30 -0600

    Correcion tabla tipo persona

--- database/migrations/2024_04_20_134350_create_tipo_persona_table.php
+++ database/migrations/2024_04_20_134350_create_tipo_persona_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('nombre');
             $table->integer('descripcion');
             $table->timestamps();
-            $table->timestamps();
         });
     }
 
