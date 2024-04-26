61e2833330e5d9b57cb8ac4c87d7163b13bb8675
r3yesmauricio <maury99900@gmail.com>
2024-04-26 16:24:28 -0600

    Funcionalidad de agregar permisos a roles y roles a usuarios, adicion de funcionalidad para agregar empleados y clientes a usuarios

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Permission;
 
 class RoleController extends Controller
 {
@@ -66,6 +67,31 @@ class RoleController extends Controller
         }
 
         response()->json(["error" => "Role is not updated"], 200);
+    }
+
+    public function assignPermissionsToRole(Request $request, $roleId)
+    {
+        // Verificar si el rol existe en la base de datos
+        $role = Role::find($roleId);
+
+        if (!$role) {
+            return response()->json(['error' => 'Role not found'], 404);
+        }
+
+        // Obtener los nombres de los permisos desde la solicitud
+        $permissionName = $request->input('permission_name');
+
+        
+        $permission = Permission::where('name', $permissionName)->first();
+
+        if ($permission) {
+            $role->givePermissionTo($permission);
+        } else {
+            // Manejar el caso en el que el permiso no existe
+            return response()->json(['message' => 'Error al agregar el permiso al rol'], 200);
+        }
+
+        return response()->json(['message' => 'Permisos agregados con exito'], 200);
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(RoleSeeder::class);
         $this->call(DepartamentoSeeder::class);
         $this->call(MunicipioSeeder::class);
     }
