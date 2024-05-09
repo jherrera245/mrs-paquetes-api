cb586900c498ad2723157ac9b99278e3d6e5ed73
jherrera245 <245jh17@gmail.com>
2024-05-08 20:22:48 -0600

    Correciones  de seeder

--- database/seeders/TipoPersonaSeeder.php
+++ database/seeders/TipoPersonaSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class TipoPersona extends Seeder
+class TipoPersonaSeeder extends Seeder
 {
     /**
      * Run the database seeds.
