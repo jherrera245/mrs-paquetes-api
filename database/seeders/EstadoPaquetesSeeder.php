01fddc3109d77189ff81f0ea02d81955c1d50a45
SAMUELROMER <48956222+SAMUELROMER@users.noreply.github.com>
2024-09-01 08:12:12 -0600

    Estado Paquetes

--- database/seeders/EstadoPaquetesSeeder.php
+++ database/seeders/EstadoPaquetesSeeder.php
@@ -10,10 +10,11 @@ class EstadoPaquetesSeeder extends Seeder
     public function run()
     {
         DB::table('estado_paquetes')->insert([
+            ['nombre' => 'En Recepción', 'descripcion' => 'El paquete está en Recepción'],
             ['nombre' => 'En Tránsito', 'descripcion' => 'El paquete está en tránsito'],
             ['nombre' => 'En Ruta de Entrega', 'descripcion' => 'El paquete está en camino para su entrega'],
             ['nombre' => 'Recibido en Destino', 'descripcion' => 'El paquete ha llegado a su destino final'],
-            ['nombre' => 'En Almacén', 'descripcion' => 'El paquete se encuentra en el almacén'],
+            ['nombre' => 'En Bodega', 'descripcion' => 'El paquete se encuentra en el Bodega'],
             ['nombre' => 'En Espera de Recolección', 'descripcion' => 'El paquete está listo para ser recolectado'],
             ['nombre' => 'Reprogramado', 'descripcion' => 'La entrega del paquete ha sido reprogramada'],
             ['nombre' => 'En Proceso de Retorno', 'descripcion' => 'El paquete está siendo devuelto al remitente'],
