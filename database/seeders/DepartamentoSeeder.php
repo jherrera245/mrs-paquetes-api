d2cedc58f6b37bf99bb786a0c301673603316f30
SAMUELROMER <48956222+SAMUELROMER@users.noreply.github.com>
2024-09-18 11:00:07 -0600

    Departamento_tildesSeder

--- database/seeders/DepartamentoSeeder.php
+++ database/seeders/DepartamentoSeeder.php
@@ -14,25 +14,37 @@ class DepartamentoSeeder extends Seeder
      */
     public function run()
     {
+        if (!function_exists('capitalizarPalabra')) {
+            /**
+             * Función personalizada para capitalizar correctamente palabras con tildes.
+             *
+             * @param string $string
+             * @return string
+             */
+            function capitalizarPalabra($string)
+            {
+                return mb_convert_case(mb_strtolower($string), MB_CASE_TITLE, "UTF-8");
+            }
+        }
+
         // Utiliza la función global `capitalizarPalabra` desde los helpers
         $data = [
-            ["nombre" => capitalizarPalabra('AHUACHAPAN')],
+            ["nombre" => capitalizarPalabra('AHUACHAPÁN')],
             ["nombre" => capitalizarPalabra('SANTA ANA')],
             ["nombre" => capitalizarPalabra('SONSONATE')],
             ["nombre" => capitalizarPalabra('CHALATENANGO')],
             ["nombre" => capitalizarPalabra('LA LIBERTAD')],
             ["nombre" => capitalizarPalabra('SAN SALVADOR')],
-            ["nombre" => capitalizarPalabra('CUSCATLAN')],
+            ["nombre" => capitalizarPalabra('CUSCATLÁN')],
             ["nombre" => capitalizarPalabra('LA PAZ')],
             ["nombre" => capitalizarPalabra('CABAÑAS')],
             ["nombre" => capitalizarPalabra('SAN VICENTE')],
-            ["nombre" => capitalizarPalabra('USULUTAN')],
+            ["nombre" => capitalizarPalabra('USULUTÁN')],
             ["nombre" => capitalizarPalabra('SAN MIGUEL')],
-            ["nombre" => capitalizarPalabra('MORAZAN')],
-            ["nombre" => capitalizarPalabra('LA UNION')],
+            ["nombre" => capitalizarPalabra('MORAZÁN')],
+            ["nombre" => capitalizarPalabra('LA UNIÓN')],
         ];
 
         DB::table('departamento')->insert($data);
     }
 }
-
