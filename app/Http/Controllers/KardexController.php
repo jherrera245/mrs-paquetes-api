65a987dcc6a821d60e2bb7b63d9e7ea22171a7a0
ReyesMauricio <u20191122@univo.edu.sv>
2024-09-28 08:53:39 -0600

    fix filtros en kardex

--- app/Http/Controllers/KardexController.php
+++ app/Http/Controllers/KardexController.php
@@ -21,32 +21,34 @@ class KardexController extends Controller
         ]);
 
         $query = DB::table('kardex as k')
-        ->join('paquetes as p', 'p.id', '=', 'k.id_paquete')
-        ->select(
-            'k.id',
-            DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', k.numero_ingreso, '') AS numero_entrada"),
-            DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', k.fecha, '') AS fecha_entrada"),
-            DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', p.uuid, '') AS paquete_entrada"),
-            DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', k.tipo_transaccion, '') AS tipo_transaccion_entrada"),
-            DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', k.cantidad, '') AS cantidad_entrada"),
-            DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', k.numero_ingreso, '') AS numero_salida"),
-            DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', k.fecha, '') AS fecha_salida"),
-            DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', p.uuid, '') AS paquete_salida"),
-            DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', k.tipo_transaccion, '') AS tipo_transaccion_salida"),
-            DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', k.cantidad, '') AS cantidad_salida")
-        )
-        ->where(function ($query) {
-            $query->where('k.tipo_movimiento', 'ENTRADA')
-                ->where('k.tipo_transaccion', 'ALMACENADO');
-        })
-        ->orWhere(function ($query) {
-            $query->where('k.tipo_movimiento', 'SALIDA')
-                ->where('k.tipo_transaccion', 'TRASLADO');
-        });
+            ->join('paquetes as p', 'p.id', '=', 'k.id_paquete')
+            ->select(
+                'k.id',
+                DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', k.numero_ingreso, '') AS numero_entrada"),
+                DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', k.fecha, '') AS fecha_entrada"),
+                DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', p.uuid, '') AS paquete_entrada"),
+                DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', k.tipo_transaccion, '') AS tipo_transaccion_entrada"),
+                DB::raw("IF(k.tipo_movimiento = 'ENTRADA' AND k.tipo_transaccion = 'ALMACENADO', k.cantidad, '') AS cantidad_entrada"),
+                DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', k.numero_ingreso, '') AS numero_salida"),
+                DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', k.fecha, '') AS fecha_salida"),
+                DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', p.uuid, '') AS paquete_salida"),
+                DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', k.tipo_transaccion, '') AS tipo_transaccion_salida"),
+                DB::raw("IF(k.tipo_movimiento = 'SALIDA' AND k.tipo_transaccion = 'TRASLADO', k.cantidad, '') AS cantidad_salida")
+            )
+            ->where(function ($query) use ($filters) {
+                if (isset($filters['uuid'])) {
+                    $query->where('p.uuid', '=', $filters['uuid']);
+                }
 
-        if (isset($filters['uuid'])) {
-            $query->where('p.uuid', 'LIKE', '%' . $filters['uuid'] . '%');
-        }
+                $query->where(function ($subQuery) {
+                    $subQuery->where('k.tipo_movimiento', 'ENTRADA')
+                        ->where('k.tipo_transaccion', 'ALMACENADO');
+                })
+                ->orWhere(function ($subQuery) {
+                    $subQuery->where('k.tipo_movimiento', 'SALIDA')
+                        ->where('k.tipo_transaccion', 'TRASLADO');
+                });
+            });
 
         if (isset($filters['tipo_movimiento'])) {
             $query->where('k.tipo_movimiento', $filters['tipo_movimiento']);
@@ -57,8 +59,6 @@ class KardexController extends Controller
             $end = Carbon::parse($filters['fecha_fin'])->format('Y-m-d');
             $query->whereBetween(DB::raw('DATE(k.fecha)'), [$start, $end]);
         }
-
-        
 
         if (isset($filters['id_orden'])) {
             $query->where('k.id_orden', $filters['id_orden']);
@@ -98,5 +98,6 @@ class KardexController extends Controller
                 'to' => $kardex->lastItem(),
             ]
         ]);
+
     }
 }
